8e6eb815c38f413440887c8b8bdf609719f07ced
luca <lucaborza@gmail.com>
2018-12-14 14:08:00 +0100

    refactoring + thread hooking

--- HookENFAL/dllmain.cpp
+++ HookENFAL/dllmain.cpp
@@ -1,11 +1,10 @@
 // dllmain.cpp : Definisce il punto di ingresso per l'applicazione DLL.
 #include "stdafx.h"
-#include "trampoline.h"
+#include "Util.h"
+#include "ThreadLibraryHooks.h"
 
 #include <iostream>
 
-LPCWSTR fname = L"c:\\Users\\luca\\ENFAL-INFO.txt";
-HANDLE debug_file;
 BOOL executed = false;
 
 #define ADDRESS_GET_EXPLORER_PID 0x402550
@@ -13,209 +12,14 @@ BOOL executed = false;
 #define ADDRESS_SKIP_REMOTE_THREAD_RETURN 0x402472
 #define ADDRESS_VIRTUAL_ALLOC 0x40138A
 
+#define ADDRESS_HOOK_THREAD 0x4013F7
+#define OFFSET_THREAD_MAIN_LOOP 0x4A9
+
 unsigned long address_t1;
 
-typedef void(*funcpointer)(void *);
-
-// UTIL ********************************************************************************************
-
-static void Message(const char* mex)
-{
-	debug_file = CreateFile(
-		fname, 
-		FILE_APPEND_DATA, 
-		FILE_SHARE_WRITE, 
-		0, 
-		OPEN_EXISTING, 
-		FILE_ATTRIBUTE_NORMAL, 
-		0);
-
-	DWORD bytesWritten;
-	WriteFile(debug_file, mex, strlen(mex), &bytesWritten, NULL);
-	CloseHandle(debug_file);
-}
-
-
-static void HookInstruction(funcpointer instructions_to_patch, funcpointer code_to_load, funcpointer return_address, unsigned char* old_data)
-{
-	DWORD bytes_written;
-
-	unsigned char opcodes[] = {									// push eax
-	0x50,														// mov eax, Trampoline
-	0xB8,														// push return_address
-	(unsigned char)(((unsigned long)&trampoline)),				// push code_to_load
-	(unsigned char)(((unsigned long)&trampoline) >> 8),			// jmp eax
-	(unsigned char)(((unsigned long)&trampoline) >> 16),
-	(unsigned char)(((unsigned long)&trampoline) >> 24),
-	0x68,
-	(unsigned char)(((unsigned long)return_address)),
-	(unsigned char)(((unsigned long)return_address) >> 8),
-	(unsigned char)(((unsigned long)return_address) >> 16),
-	(unsigned char)(((unsigned long)return_address) >> 24),
-	0x68,
-	(unsigned char)(((unsigned long)code_to_load)),
-	(unsigned char)(((unsigned long)code_to_load) >> 8),
-	(unsigned char)(((unsigned long)code_to_load) >> 16),
-	(unsigned char)(((unsigned long)code_to_load) >> 24),
-	0xFF,
-	0xE0
-	};
-	SIZE_T len_opcodes = sizeof(opcodes);
-
-	DWORD dwProtect;
-	if (!VirtualProtect(instructions_to_patch, len_opcodes, PAGE_EXECUTE_READWRITE, &dwProtect)) {
-		Message("VirtualProtect failed\n");
-		exit(1);
-	}
-
-	// Save old opcodes
-	if (!WriteProcessMemory(
-		GetCurrentProcess(),
-		(LPVOID)old_data,
-		(LPVOID)instructions_to_patch,
-		len_opcodes,
-		&bytes_written
-	)) {
-		Message("WriteProcessMemory failed\n");
-		exit(1);
-	}
-	else if (bytes_written != len_opcodes) {
-		Message("written too few bytes\n");
-		exit(1);
-	}
-
-	// Write new opcodes
-	if (!WriteProcessMemory(
-		GetCurrentProcess(),
-		(LPVOID)instructions_to_patch,
-		(LPVOID)opcodes,
-		len_opcodes,
-		&bytes_written
-	)) {
-		Message("WriteProcessMemory failed\n");
-		exit(1);
-	}
-	else if (bytes_written != len_opcodes) {
-		Message("written too few bytes\n");
-		exit(1);
-	}
-
-	if (!FlushInstructionCache(
-		GetCurrentProcess(),
-		instructions_to_patch,
-		len_opcodes
-	)) {
-		Message("FlushInstructionCache failed\n");
-		exit(1);
-	}
-
-}
-
-static void HookFunction(funcpointer address_to_patch, funcpointer function_to_load, unsigned char* old_data)
-{
-	DWORD bytes_written;
-
-	unsigned char opcodes[] = {										// MOV EAX, $ADDRESS_TO_PATCH
-		0xB8,														// JMP EAX
-		(unsigned char)(((unsigned long)function_to_load)),
-		(unsigned char)(((unsigned long)function_to_load) >> 8),
-		(unsigned char)(((unsigned long)function_to_load) >> 16),
-		(unsigned char)(((unsigned long)function_to_load) >> 24),
-		0xFF,
-		0xE0
-	};
-	SIZE_T len_opcodes = sizeof(opcodes);
-
-	DWORD dwProtect;
-	if (!VirtualProtect(address_to_patch, len_opcodes, PAGE_EXECUTE_READWRITE, &dwProtect)) {
-		Message("VirtualProtect failed\n");
-		exit(1);
-	}
-
-	// Save old opcodes
-	if (!WriteProcessMemory(
-		GetCurrentProcess(),
-		(LPVOID)old_data,
-		(LPVOID)address_to_patch,
-		len_opcodes,
-		&bytes_written
-	)) {
-		Message("WriteProcessMemory failed\n");
-		exit(1);
-	}
-	else if (bytes_written != len_opcodes) {
-		Message("written too few bytes\n");
-		exit(1);
-	}
-
-	// Write new opcodes
-	if (!WriteProcessMemory(
-		GetCurrentProcess(),
-		(LPVOID)address_to_patch,
-		(LPVOID)opcodes,
-		len_opcodes,
-		&bytes_written
-	)) {
-		Message("WriteProcessMemory failed\n");
-		exit(1);
-	}
-	else if (bytes_written != len_opcodes) {
-		Message("written too few bytes\n");
-		exit(1);
-	}
-
-	if (!FlushInstructionCache(
-		GetCurrentProcess(),
-		address_to_patch,
-		len_opcodes
-	)) {
-		Message("FlushInstructionCache failed\n");
-		exit(1);
-	}
-}
-
-static void HookDynamicFunction(LPCSTR module_name, LPCSTR function_name, funcpointer function_to_load, unsigned char* old_data) 
-{
-	funcpointer f = (funcpointer)GetProcAddress(GetModuleHandleA(module_name), function_name);
-	if ( !f ) {
-		Message("GetProcAddress failed\n");
-		exit(1);
-	}
-	HookFunction(f, function_to_load, old_data);
-}
-
-// restore old opcodes
-static void RestoreData(LPVOID dst, LPVOID src, DWORD len)
-{
-	DWORD bytes_written;
-	if (!WriteProcessMemory(
-		GetCurrentProcess(),
-		(LPVOID)dst,
-		(LPVOID)src,
-		len,
-		&bytes_written
-	)) {
-		Message("WriteProcessMemory failed\n");
-		exit(1);
-	}
-	else if (bytes_written != len) {
-		Message("written too few bytes\n");
-		exit(1);
-	}
-
-	if (!FlushInstructionCache(
-		GetCurrentProcess(),
-		dst,
-		len
-	)) {
-		Message("FlushInstructionCache failed\n");
-		exit(1);
-	}
-}
-
-// *************************************************************************************************
 // HOOKS *******************************************************************************************
 
+// return PID of enfal instead of PID of explorer.exe
 unsigned char oldGetExplorerPid[7] = { 0 };
 static DWORD HookGetExplorerPid()
 {
@@ -224,6 +28,7 @@ static DWORD HookGetExplorerPid()
 	return GetCurrentProcessId();
 }
 
+// avoid SetReg
 unsigned char oldSetReg[7] = { 0 };
 static LSTATUS HookSetReg()
 {
@@ -232,6 +37,7 @@ static LSTATUS HookSetReg()
 	return 1;
 }
 
+// avoid CloseReg (crash otherwise)
 unsigned char oldCloseReg[7] = { 0 };
 static LSTATUS HookCloseReg()
 {
@@ -240,6 +46,7 @@ static LSTATUS HookCloseReg()
 	return 1;
 }
 
+// create only the first thread (the interesting one) and put the main thread to sleep
 unsigned char oldSkipRemote[18] = { 0 };
 static void SkipOtherCreateRemoteThread(unsigned long eax, unsigned long ebx, unsigned long ecx, 
 	unsigned long edx, unsigned long edi, unsigned long esi)
@@ -249,19 +56,47 @@ static void SkipOtherCreateRemoteThread(unsigned long eax, unsigned long ebx, un
 	Sleep(1000 * 3600); // sleep for 1 hour
 }
 
+// intercept the address of the function where the thread will start
 unsigned char oldInterceptVirtualAlloc[18] = { 0 };
 static void InterceptVirtualAlloc(unsigned long eax, unsigned long ebx, unsigned long ecx,
 	unsigned long edx, unsigned long edi, unsigned long esi)
 {
 	address_t1 = eax;
 
-	char str[10]; str[9] = '\0';
-	sprintf_s(str, "%08x\n", address_t1);
-	Message("InterceptVirtualAlloc triggered\n");
-	Message("ADDRESS: ");
-	Message(str);
-
+	Message("InterceptVirtualAlloc triggered, ADDRESS: %08x\n", address_t1);
 	RestoreData((LPVOID)ADDRESS_VIRTUAL_ALLOC, oldInterceptVirtualAlloc, 18);
+}
+
+// hook the thread function
+unsigned char oldThreadFunction[18] = { 0 };
+static void InterceptThreadFunction(unsigned long eax, unsigned long ebx, unsigned long ecx,
+	unsigned long edx, unsigned long edi, unsigned long esi)
+{
+	Message("Thread function triggered\n");
+	RestoreData((LPVOID)address_t1, oldThreadFunction, 18);
+}
+
+unsigned char oldThreadInitializationFinished[18] = { 0 };
+static void ThreadInitializationFinished(unsigned long eax, unsigned long ebx, unsigned long ecx,
+	unsigned long edx, unsigned long edi, unsigned long esi)
+{
+	Message("Thread initialization phase finished. Entering the main loop. Hooking library functions.\n");
+	RestoreData((LPVOID)(address_t1 + OFFSET_THREAD_MAIN_LOOP), oldThreadInitializationFinished, 18);
+	HookDynamicFunction("wininet", "InternetOpenA" , (funcpointer)&HookInternetOpenA, oldHookInternetOpenA);
+}
+
+// set the hooks of the thread function (using the address leaked by InterceptVirtualAlloc)
+unsigned char oldWriteThreadHook[18] = { 0 };
+static void WriteThreadHook(unsigned long eax, unsigned long ebx, unsigned long ecx,
+	unsigned long edx, unsigned long edi, unsigned long esi)
+{
+	Message("Writing on thread function data\n");
+	RestoreData((LPVOID)ADDRESS_HOOK_THREAD, oldWriteThreadHook, 18);
+	HookInstruction((funcpointer)address_t1, (funcpointer)&InterceptThreadFunction,
+		(funcpointer)address_t1, oldThreadFunction);
+	HookInstruction((funcpointer)(address_t1+OFFSET_THREAD_MAIN_LOOP), (funcpointer)&ThreadInitializationFinished,
+		(funcpointer)(address_t1+OFFSET_THREAD_MAIN_LOOP), oldThreadInitializationFinished);
+
 }
 
 // *************************************************************************************************
@@ -273,7 +108,7 @@ BOOL APIENTRY DllMain(HMODULE hModule,
 {
 	if (!executed) {
 		// Create a file with the given information...
-		debug_file = CreateFile(fname, // file to be opened
+		HANDLE debug_file = CreateFile(fname, // file to be opened
 			GENERIC_WRITE, // open for writing
 			FILE_SHARE_WRITE, // share for writing
 			NULL, // default security
@@ -289,6 +124,8 @@ BOOL APIENTRY DllMain(HMODULE hModule,
 			(funcpointer)ADDRESS_SKIP_REMOTE_THREAD_RETURN, oldSkipRemote);
 		HookInstruction((funcpointer)ADDRESS_VIRTUAL_ALLOC, (funcpointer)&InterceptVirtualAlloc, 
 			(funcpointer)ADDRESS_VIRTUAL_ALLOC, oldInterceptVirtualAlloc);
+		HookInstruction((funcpointer)ADDRESS_HOOK_THREAD, (funcpointer)&WriteThreadHook,
+			(funcpointer)ADDRESS_HOOK_THREAD, oldWriteThreadHook);
 		HookDynamicFunction("advapi32", "RegSetValueExA", (funcpointer)&HookSetReg, oldSetReg);
 		HookDynamicFunction("advapi32", "RegCloseKey", (funcpointer)&HookCloseReg, oldCloseReg);
 		Message("Patch completed\n");
--- HookENFAL/stdafx.h
+++ HookENFAL/stdafx.h
@@ -10,6 +10,9 @@
 #define WIN32_LEAN_AND_MEAN             // Escludere gli elementi usati raramente dalle intestazioni di Windows
 // File di intestazione di Windows
 #include <windows.h>
+#include <cstdlib>
+#include <iostream>
+
 
 
 
